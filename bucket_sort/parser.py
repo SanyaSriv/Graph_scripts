2851b486604a280c83b3d70d7567c3e527492700
SanyaSriv <ssrivas7@ucsc.edu>
2023-01-15 02:59:57 -0800

    Adding parser file to read benchmark data

---
+++ bucket_sort/parser.py
@@ -0,0 +1,35 @@
+import re
+import sys
+
+meta_data = {}
+
+NUMBER_OF_SEGMENTS = 24
+MATCH_KERNEL = "  kernel:"
+MATCH_MILLIS = "        millis:"
+
+benchmark_file = open(sys.argv[-1], 'r');
+
+while True:
+    line = benchmark_file.readline()
+    # reached the end of file
+    if len(line) == 0:
+        break
+    if re.match(MATCH_KERNEL, line) != None:
+        # we matched with a kernel name
+        name_of_kernel = " ".join(line.split()[1::])
+        meta_data[name_of_kernel] = []
+        # print(name_of_kernel)
+        number_of_millis = 0
+        while number_of_millis < NUMBER_OF_SEGMENTS:
+            line = benchmark_file.readline()
+            if re.match(MATCH_MILLIS, line) != None:
+                number_of_millis += 1
+                millis = line.split()[-1]
+                meta_data[name_of_kernel].append(millis)
+                # print(millis)
+print(meta_data)
+
+        
+
+
+
