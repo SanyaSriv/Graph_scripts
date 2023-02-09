bdb8e9971c3c734dcb57de0fbad4f9761aff38c8
SanyaSriv <ssrivas7@ucsc.edu>
2023-02-08 22:30:04 -0800

    Added the function for printing schedules with cost

--- bucket_sort/bucket_sort.cpp
+++ bucket_sort/bucket_sort.cpp
@@ -79,6 +79,68 @@ void schedule_printer(int number_of_buckets, string kernel_name[], int bucket_ra
   cout << endl; 
 }
 
+void schedule_printer_cost(int dynamic_bucket_size, string kernel_name[], float bucket_cost[], int bucket_range[]) {
+  for (int indi = 0; indi < 9; indi++) {
+    std::cout << "----------";
+  }
+  std::cout << endl;
+  int mid_size = 30;
+  string header_string = "Bucket name + range";
+  std::cout << "Bucket name + range";
+  int temp_space = mid_size - header_string.size();
+  for (int indi = 0; indi < temp_space; indi++) {
+    cout << " ";
+  }
+  cout << "|";
+  std::cout << "     Kernel Name";
+  header_string = "Kernel Name";
+  temp_space = mid_size - header_string.size();
+  for (int indi = 0; indi < temp_space; indi++) {
+    cout << " ";
+  }
+  std::cout << "|      Bucket Cost" << endl;
+
+  for (int i = 0; i < dynamic_bucket_size; i++) {
+    for (int indi = 0; indi < 9; indi++) {
+      std::cout << "----------";
+    }
+    std::cout << endl;
+    string bucket = "Bucket" + to_string(i);
+    int temp_space_var = mid_size - bucket.length();
+    std::cout << bucket;
+    for (int indi = 0; indi < temp_space_var; indi++) {
+      cout << " ";
+    }
+    cout << "|     ";
+
+    cout << kernel_name[i];
+    temp_space_var = mid_size - kernel_name[i].size();
+    for (int indi = 0; indi < temp_space_var; indi++) {
+      cout << " ";
+    }
+    cout << "|     ";
+
+    // putting the kernel cost in here
+    cout << bucket_cost[i] << endl;
+
+    bucket = to_string(bucket_range[i]) + " - " + to_string(bucket_range[i + 1]);
+    temp_space_var = mid_size - bucket.length();
+    std::cout << bucket;
+    for (int indi = 0; indi < temp_space_var; indi++)
+    {
+      cout << " ";
+    }
+    cout << "|     ";
+
+    temp_space_var = mid_size - kernel_name[i].size();
+    for (int indi = 0; indi < mid_size; indi++) {
+      cout << " ";
+    }
+    cout << "|     " << endl;
+  }
+  cout << endl;
+}
+
 map <string, vector <float> > benchmark_data_map;
 
 int main(int argc, char** argv) {
@@ -269,11 +331,6 @@ int main(int argc, char** argv) {
   std::cout << "Buckets created + populated: Success." << endl;
   std::cout << "Reduction Process beginning: Success." << endl;
   std::cout <<"Current bucket size = " << dynamic_bucket_size << endl; // works till here - Success seen on Toucan
-  // std::cout << "Number of nodes in buckets = " << endl;
-  // for(int i = 0; i < number_of_buckets; i++) {
-  //   bucket_size[i] = graph_bucket_list_nodes[i].size();
-  //   std::cout << to_string(i) << "  " << to_string(bucket_size[i]) << endl;
-  // }
 
 // 200 180 140 50 70 80 0
   int ideal = 6;
@@ -357,67 +414,7 @@ int main(int argc, char** argv) {
 
       // if a change was made then we will print out the schedule
       std::cout << "Printing out the schedule -> " << endl;
-      // technically, we should be calling a function here but for now, I am writing the code in the main
-      for(int indi = 0; indi < 9; indi ++) {
-        std::cout << "----------";
-      }
-      std::cout << endl;
-      int mid_size = 30;
-      string header_string = "Bucket name + range";
-      std::cout << "Bucket name + range";
-      int temp_space = mid_size - header_string.size();
-      for(int indi = 0; indi < temp_space; indi ++) {
-        cout << " ";
-      }
-      cout << "|";
-      std::cout << "     Kernel Name";
-      header_string = "Kernel Name";
-      temp_space = mid_size - header_string.size();
-      for(int indi = 0; indi < temp_space; indi ++) {
-        cout << " ";
-      }
-      std::cout << "|      Bucket Cost" << endl;
-      
-      for(int i = 0; i < dynamic_bucket_size; i++) {
-        for(int indi = 0; indi < 9; indi ++) {
-          std::cout << "----------";
-        }
-        std::cout << endl;
-        string bucket = "Bucket" + to_string(i);
-        int temp_space_var = mid_size - bucket.length();
-        std::cout << bucket;
-        for(int indi = 0; indi < temp_space_var; indi ++) {
-          cout << " ";
-        }
-        cout << "|     ";
-
-        cout << kernel_name[i];
-        temp_space_var = mid_size - kernel_name[i].size();
-         for(int indi = 0; indi < temp_space_var; indi ++) {
-          cout << " ";
-        }
-        cout << "|     ";
-
-        // putting the kernel cost in here
-        cout << bucket_cost[i] << endl;
-
-        bucket = to_string(bucket_range[i]) + " - " + to_string(bucket_range[i + 1]);
-        temp_space_var = mid_size - bucket.length();
-        std::cout << bucket;
-        for(int indi = 0; indi < temp_space_var; indi ++) {
-          cout << " ";
-        }
-        cout << "|     ";
-
-      temp_space_var = mid_size - kernel_name[i].size();
-      for(int indi = 0; indi < mid_size; indi ++) {
-        cout << " ";
-      }
-      cout << "|     " << endl;
-
-      }
-      cout << endl;
-      // printing ends here
+      schedule_printer_cost(dynamic_bucket_size, kernel_name, bucket_cost, bucket_range);
     }
 
     // now start merging by the bucket cost and benchmark data
@@ -549,71 +546,9 @@ int main(int argc, char** argv) {
           // now we need to merge the array of vector
           dynamic_bucket_size -=1;
       }
-
       std::cout << "Printing out the schedule -> " << endl;
-      // technically, we should be calling a function here but for now, I am writing the code in the main
-      for(int indi = 0; indi < 9; indi ++) {
-        std::cout << "----------";
-      }
-      std::cout << endl;
-      int mid_size = 30;
-      string header_string = "Bucket name + range";
-      std::cout << "Bucket name + range";
-      int temp_space = mid_size - header_string.size();
-      for(int indi = 0; indi < temp_space; indi ++) {
-        cout << " ";
-      }
-      cout << "|";
-      std::cout << "     Kernel Name";
-      header_string = "Kernel Name";
-      temp_space = mid_size - header_string.size();
-      for(int indi = 0; indi < temp_space; indi ++) {
-        cout << " ";
-      }
-      std::cout << "|      Bucket Cost" << endl;
-      
-      for(int i = 0; i < dynamic_bucket_size; i++) {
-        for(int indi = 0; indi < 9; indi ++) {
-          std::cout << "----------";
-        }
-        std::cout << endl;
-        string bucket = "Bucket" + to_string(i);
-        int temp_space_var = mid_size - bucket.length();
-        std::cout << bucket;
-        for(int indi = 0; indi < temp_space_var; indi ++) {
-          cout << " ";
-        }
-        cout << "|     ";
-
-        cout << kernel_name[i];
-        temp_space_var = mid_size - kernel_name[i].size();
-         for(int indi = 0; indi < temp_space_var; indi ++) {
-          cout << " ";
-        }
-        cout << "|     ";
-
-        // putting the kernel cost in here
-        cout << bucket_cost[i] << endl;
-
-        bucket = to_string(bucket_range[i]) + " - " + to_string(bucket_range[i + 1]);
-        temp_space_var = mid_size - bucket.length();
-        std::cout << bucket;
-        for(int indi = 0; indi < temp_space_var; indi ++) {
-          cout << " ";
-        }
-        cout << "|     ";
-
-      temp_space_var = mid_size - kernel_name[i].size();
-      for(int indi = 0; indi < mid_size; indi ++) {
-        cout << " ";
-      }
-      cout << "|     " << endl;
-
-      }
-      cout << endl;
-
+      schedule_printer_cost(dynamic_bucket_size, kernel_name, bucket_cost, bucket_range);
     }
-
   } else if (to_do_flag == 1) {
    
   }
